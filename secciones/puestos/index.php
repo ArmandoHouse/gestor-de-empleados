1fbb29ca7930bb5838970457ec8e4c7477097a37
armandohouse <emilianostyles10@gmail.com>
2023-10-31 20:11:16 -0300

    funcionalidad del index de puestos

--- secciones/puestos/index.php
+++ secciones/puestos/index.php
@@ -1,3 +1,12 @@
+<?php 
+include("../../bd.php");
+
+$sentencia=$conexion->prepare("SELECT * FROM `puestos`");
+$sentencia->execute();
+
+$lista_puestos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 <?php include("../../templates/header.php"); ?>
 
 <br>
@@ -21,14 +30,19 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="">
-                        <td scope="row">1</td>
-                        <td>Programador Jr.</td>
-                        <td>
-                            <input name="btneditar" id="btneditar" class="btn btn-primary" type="button" value="Editar">
-                            <input name="btneliminar" id="btneliminar" class="btn btn-danger" type="button" value="Eliminar">
-                        </td>
-                    </tr>
+                    <?php foreach ($lista_puestos as $registro) {?>
+
+                        <tr class="">
+                            <td scope="row"><?php echo $registro['id'];?></td>
+                            <td><?php echo $registro['nombreDelPuesto'];?></td>
+                            <td>
+                                <input name="btneditar" id="btneditar" class="btn btn-primary" type="button" value="Editar">
+                                <input name="btneliminar" id="btneliminar" class="btn btn-danger" type="button" value="Eliminar">
+                            </td>
+                        </tr>
+
+                        <?php } ?>
+                    
                 </tbody>
             </table>
         </div>
