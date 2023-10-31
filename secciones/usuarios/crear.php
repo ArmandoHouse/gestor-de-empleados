58db19d1ab0edd46a16410afe048dbce935127fd
armandohouse <emilianostyles10@gmail.com>
2023-10-31 19:00:39 -0300

    vistas del index y crear usuarios

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,5 +1,36 @@
 <?php include("../../templates/header.php"); ?>
 
-crear usuarios
+<br>
+
+<div class="card">
+    <div class="card-header">
+        Datos del usuario
+    </div>
+    <div class="card-body">
+        
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+              <label for="usuario" class="form-label">Nombre del usuario</label>
+              <input type="text"
+                class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Usuario">
+            </div>
+            <div class="mb-3">
+              <label for="correo" class="form-label">Correo</label>
+              <input type="email"
+                class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="Correo">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Contraseña</label>
+              <input type="password"
+                class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Contraseña">
+            </div>
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a name="" id="" class="btn btn-danger" href="index.php" role="button">Cancelar</a>
+        </form>
+
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
 
 <?php include("../../templates/footer.php"); ?>
